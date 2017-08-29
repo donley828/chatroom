8af63195851bf000b682cff60ca06643f96fcc0b
donley828 <344919627@qq.com>
2017-08-29 20:42:27 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+//保存所有在线用户
+var users = [];
+
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);//将socke模块与服务器进行绑定
+
+
+app.use('/', express.static(__dirname + '/www'));
+server.listen(3000);
+
+//socket
+io.on('connection', function(socket) {
+    //设置picknamae
+    socket.on('login', function(nickname) {
+        if (users.indexOf(nickname) > -1) {
+            socket.emit('nickExisted');
+        } else {
+            socket.userIndex = users.length;
+            socket.nickname = nickname;
+            users.push(nickname);
+            socket.emit('loginSuccess');
+            io.sockets.emit('system', nickname, users.length, 'login'); //向所有连接到服务器的客户发送当前登录的用户
+        }
+    });
+    //用户离开
+    socket.on('disconnect', function() {
+        users.splice(socket.userIndex, 1);
+        //通知其他人
+        socket.broadcast.emit('system', socket.nickname, users.length, 'logout');
+    });
+    //接收消息
+    socket.on('postMsg', function(msg) {
+        //显示
+        socket.broadcast.emit('newMsg', socket.nickname, msg);
+    });
+}); 
+
+
---
+++ www/js/chat.js
@@ -0,0 +1,87 @@
+window.onload = function () {
+    var chat = new Chat();
+    chat.init();
+}
+
+//定义Chat类
+var Chat = function () {
+    this.socket = null;
+}
+
+Chat.prototype = {
+    init: function () { //初始化
+        var that = this;
+        //建立与服务器的socket的连接
+        this.socket = io.connect();
+        //发送按钮事件
+        var sendBtn = document.getElementById('sendBtn');
+        sendBtn.addEventListener('click', function () {
+            var messageInput = document.getElementById('input-area'),
+                msg = messageInput.value;
+            messageInput.value = '';
+            messageInput.focus();
+            if (msg.trim().length != 0) {
+                that.socket.emit('postMsg', msg); //发送消息给服务器
+                that._displayNewMsg('Me', msg); //自己窗口显示
+            }
+        }, false);
+        EventUtil.addHandler(sendBtn, "keyup", function(event) {}); //绑定键码
+
+        //图片发送
+        document.getElementById('imgBtn').addEventListener('change', function() {
+            //检查是否有图片选中
+            if (this.files.lenght != 0) {
+                
+            }
+        });
+
+        //监听connect事件
+        this.socket.on('connect', function () {
+            document.getElementById('info').style.display = 'none';
+            document.getElementById('login').style.display = 'block';
+            document.getElementById('nickstr').focus();
+            document.getElementById('connBtn').addEventListener('click', function () {
+                var nickName = document.getElementById('nickstr').value;
+
+                if (nickName.trim().length != 0) {
+                    that.socket.emit('login', nickName);
+                } else {
+                    document.getElementById('nickstr').focus();
+                }
+            }, false);
+        });
+        this.socket.on('nickExisted', function () {
+            alert('昵称已被占用');
+        })
+        this.socket.on('loginSuccess', function () {
+            document.title = "Hello, " + document.getElementById('nickstr').value;
+            document.getElementById('loginpanel').style.display = 'none';
+            document.getElementById('input-area').focus();
+        });
+        this.socket.on('system', function (nickName, userCount, type) {
+            //判断用户是离开还是登录
+            var msg = nickName + (type == 'login' ? ' Joined' : ' Left');
+            if (msg === "null Left")
+                return false;
+            that._displayNewMsg('System', msg, '#B43127');
+            // var p = document.createElement('p');
+            // p.textContent = msg;
+            // document.getElementById('msg').appendChild(p);
+            //显示在线人数
+            document.getElementById('status').textContent = userCount + (userCount > 1 ? ' users' : ' user') + ' online';
+        });
+        this.socket.on('newMsg', function (user, msg) {
+            that._displayNewMsg(user, msg);
+        });
+
+    },
+    _displayNewMsg: function (user, msg, color) {
+        var container = document.getElementById('msg'),
+            msgToDisplay = document.createElement('p'),
+            date = new Date().toTimeString().substr(0, 8);
+        msgToDisplay.style.color = color || '#000';
+        msgToDisplay.innerHTML = user + '<span class="timespan">(' + date + '): </span>' + msg;
+        container.appendChild(msgToDisplay);
+        container.scrollTop = container.scrollHeight;
+    }
+}
